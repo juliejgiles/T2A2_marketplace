3c23ad505800521136cfc986f9df7c9e89c3393a
JulieGiles <julie_j_giles@hotmail.com>
2020-11-19 10:59:35 +1100

    Removed webpacker from app to address heroku deployment failure

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,8 +9,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("packs/stickers")
-require("packs/global")
+// require("packs/stickers")
+// require("packs/global")
 //= require jquery3
 //= require popper
 //= require bootstrap
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,13 +1,13 @@
-const { environment } = require('@rails/webpacker')
+// const { environment } = require('@rails/webpacker')
 
 //adds the webpacker library
-const webpack = require("webpack")
+// const webpack = require("webpack")
 
 //appends the below "Provide" plugin to our loader, and allows Bootstrap to access the $ variable, 'jquery' and popper
-environment.plugins.append("Provide", new webpack.ProvidePlugin({
-    $: 'jquery/src/jquery', 
-    jQuery: 'jquery/src/jquery', 
-    Popper: ['popper.js', 'default']
-}))
+// environment.plugins.append("Provide", new webpack.ProvidePlugin({
+//     $: 'jquery/src/jquery', 
+//     jQuery: 'jquery/src/jquery', 
+//     Popper: ['popper.js', 'default']
+// }))
 
-module.exports = environment
+// module.exports = environment
